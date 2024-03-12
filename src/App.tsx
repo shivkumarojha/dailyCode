50cc97b723a57e367922b806a34ded652aae76c8
shivkumarojha <ojhashivkumar88@gmail.com>
2024-03-12 22:27:02 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Nav from './components/Nav'
+function App() {
+  return (
+    <div>
+      <Nav />
+      <p className='text-'>Shiv Kumar ojha</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.tsx
@@ -0,0 +1,9 @@
+export default function Nav() {
+    return (
+      <div className="flex">
+        <div className="">Daily Code</div>
+        <button className="">Log Out</button>
+        <button>themes</button>
+      </div>
+    );
+}
